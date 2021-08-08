a3ea6d9ced84f3f6b6390ab9305010725e970ffe
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-08-08 17:23:56 +0300

    Add navigation bar with a create goal button

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goalpost-app
 //
 //  Created by Hasan Elhussein on 8.08.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
