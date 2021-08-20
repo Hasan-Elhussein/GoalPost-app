4ef83e02b09d2bc11c9425d90358ee09de4812cc
Hasan-Elhussein <hasan.elhussein.97@gmail.com>
2021-08-20 23:51:17 +0300

    Add UIButtonExt to set buttons background colors when selected or deselected

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -14,17 +14,28 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        nextBtn.bindToKeyboard()
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     
     @IBAction func nextBtnIsTapped(_ sender: Any) {
     }
     @IBAction func shortTermBtnIsTapped(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        longTermBtn.setDeselectedColor()
     }
     @IBAction func longTermBtnIsTapped(_ sender: Any) {
+        goalType = .longTerm
+        longTermBtn.setSelectedColor()
+        shortTermBtn.setDeselectedColor()
     }
     @IBAction func backBtnIsTapped(_ sender: Any) {
         dismissDetail()
--- goalpost-app/Extensions/UIViewExt.swift
+++ goalpost-app/Extensions/UIViewExt.swift
@@ -10,21 +10,20 @@ import UIKit
 extension UIView {
     func bindToKeyboard() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
         
-        func keyboardWillChange(_ notification: NSNotification){
+    @objc func keyboardWillChange(_ notification: NSNotification){
             let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
             let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
             let startingFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
             let endingFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
             
-            let deltaY = endingFrame.origin.y = startingFrame.origin.y
+            let deltaY = endingFrame.origin.y - startingFrame.origin.y
             
             UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: KeyframeAnimationOptions(rawValue: curve), animations: {
                 self.frame.origin.y += deltaY
             }, completion: nil)
             
-            
         }
-    }
     
 }
